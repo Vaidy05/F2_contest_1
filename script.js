da8a38f92056e736ba06ace0128826d4146d7be6
Vaidyanathan Valliappan <vaidyanathan589@gmail.com>
2023-04-15 21:16:08 +0530

    Commit1

--- script.js
+++ script.js
@@ -8,20 +8,51 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(function(index){
+    if(index.profession=="developer")
+       console.log(index);
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(function(index){
+    if(index.profession=="developer")
+       console.log(index);
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  let obj = {id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(obj);
+  console.log(arr[arr.length-1]);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let index=new Array();
+  for(let i=0;i<arr.length;i++){
+      if(arr[i].profession=='admin')
+        index.push(arr[i]);
+  }
+ 
+  for(let i=0;i<index.length;i++){
+      
+      let j = arr.indexOf(index[i]);
+      arr.splice(j,1);
+  }
+
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let item = [
+  { id: 4, name: "dhoni", age: "41", profession: "admin" },
+  { id: 5, name: "virat", age: "34", profession: "developer" },
+  { id: 6, name: "rohit", age: "35", profession: "admin" },
+];
+let array=arr.concat(item);
+console.log(array);
 }
